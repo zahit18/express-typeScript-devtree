4a990b4ec0da54258aa5be44ce2b78033017ca4f
zahit18 <zahithdez@gmail.com>
2025-04-26 22:51:41 -0700

    Config Cors

--- src/sever.ts
+++ src/sever.ts
@@ -1,11 +1,17 @@
 import express from "express";
 import 'dotenv/config'
+import cors from "cors";
+
 import router from "./router";
 import { connectDB } from "./config/db";
+import { corsConfig } from "./config/cors"
+
+connectDB()
 
 const app = express()
 
-connectDB()
+// Cors
+app.use(cors(corsConfig))
 
 // Leer datos de formularios
 app.use(express.json())
